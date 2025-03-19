f69aa120e2ecc85aecdaa812cecbdd734e87f9b6
Carlos <carlos746746@yahoo.com>
2025-03-19 16:17:15 -0700

    fingers crossed

--- todolist17Test.java
+++ todolist17Test.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit tests for the TodoList class.
  */
-public class Todolist17Test {
+public class todolist17Test {
     private TodoList todoList;
 
     @BeforeEach
